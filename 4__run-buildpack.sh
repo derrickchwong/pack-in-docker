f5d51efcfc0368b1db4e960797b673db4f34f7bf
Derrick Wong <derrick.chwong@gmail.com>
2020-04-23 23:45:15 +0800

    add docker login

--- 4__run-buildpack.sh
+++ 4__run-buildpack.sh
@@ -3,4 +3,4 @@ set -ex
 dir=`pwd`
 docker run -v /tmp:/tmp \
 -v /var/run/docker.sock:/var/run/docker.sock \
--v $dir/dotnet3/SampleWebApi:/workspaces/SampleWebApi derrickchwong/cnb-dotnet-pack sh -c "pack build harbor.derrickwong.hk/apps/sample-web-api:latest --path /workspaces/SampleWebApi/published --builder cloudfoundry/cnb:cflinuxfs3 --buildpack /app/dotnet-core-cnb-cf ; docker push harbor.derrickwong.hk/apps/sample-web-api:latest"
+-v $dir/dotnet3/SampleWebApi:/workspaces/SampleWebApi derrickchwong/cnb-dotnet-pack sh -c "pack build harbor.derrickwong.hk/apps/sample-web-api:latest --path /workspaces/SampleWebApi/published --builder cloudfoundry/cnb:cflinuxfs3 --buildpack /app/dotnet-core-cnb-cf ; docker login http://harbor.derrickwong.hk -u admin -p passssss ; docker push harbor.derrickwong.hk/apps/sample-web-api:latest"
